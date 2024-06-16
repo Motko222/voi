98b60c0f30bbb43e31556af8a4264b314e7642a2
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-16 23:52:12 +0200

    Update create-service.sh

--- create-service.sh
+++ create-service.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo tee /etc/systemd/system/availightd.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/nubit-lightd.service > /dev/null <<EOF
 [Unit]
-Description=Avail Light Client
+Description=Nubit Light Client
 After=network.target
 StartLimitIntervalSec=0
 [Service]
 User=root
-ExecStart=/root/scripts/avail-light/start-light.sh
+ExecStart=/root/scripts/nubit-light/start-light.sh
 Restart=always
 RestartSec=30
 [Install]
@@ -15,6 +15,4 @@ WantedBy=multi-user.target
 EOF
 
 sudo systemctl daemon-reload
-sudo systemctl enable availightd
-
-echo "Service created, start with start-service.sh"
+sudo systemctl enable nubit-lightd
