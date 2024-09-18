fdcfecf1049fa9302f3f1b68c7c07f5c1905bb8b
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-18 22:09:03 +0200

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-container=$(docker ps | grep voinetwork/docker-participation-node | awk '{print $NF}')
+container=$(docker ps | grep -E "voinetwork/docker-participation-node|voinetwork/voi-node" | awk '{print $NF}')
 docker logs --tail 100 -f $container
