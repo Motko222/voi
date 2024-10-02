d2658ece5923060b5617bec46a22bf866c5ea794
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-02 12:06:23 +0200

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,13 @@
+#~/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+
+read -p "Sure? " sure
+case $sure in  y|Y|yes|YES|Yes) ;; *) exit ;; esac
+
+docker swarm leave --force
+rm -rf ~/voi/
+rm -rf /var/lib/voi
+rm -rf /scripts/$folder
+bash /scripts/system/delete-id.sh $folder
