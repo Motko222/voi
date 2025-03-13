f7a29e76bee75eacdbc51bc7593cad382becfb5c
Motko <77466238+Motko222@users.noreply.github.com>
2025-03-13 21:25:08 +0100

    Update go-online.sh

--- go-online.sh
+++ go-online.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+cd $path
+source config
 
 cd ~/voi/bin
-./go-online
+./go-online $WALLET
 
